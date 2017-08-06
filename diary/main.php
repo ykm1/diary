4f4febe492b8cc31255a56417dd0aef5708c101c
ykm <ykm5708@gmail.com>
2017-08-06 20:15:52 +0900

    remove commit

--- diary/main.php
+++
@@ -1,3 +0,0 @@
-<?php
-print "<a href='index.php?action=list'><h2>My Diary</h2></a>";
-?>
--- diary/new.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<h4>중복체크</h4>
-<form name="f1" action="index.php?action=check" method="post">
-날짜:<input type="text" name="fname">
-<input type="submit" value="중복체크">
-</form>
-</body>
-</html>
